f50189ffd571a43703eca13b40cc7d1d6ac304fd
jinhuizxc <1004260403@qq.com>
2018-01-25 09:21:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jinhui/cityselector/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jinhui.cityselector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 看到一个不错的城市选择器，学习下！
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
